7fc67356609772de2a6b4924036bf175e0766b39
samade747 <samad.e747@gmail.com>
2024-01-20 13:17:39 +0500

    117pm 20 1 24

---
+++ app.js
@@ -0,0 +1,31 @@
+//  Assignment  1 
+// chapter 1
+
+let age = 25
+let is_student = true
+
+console.log('Age'+ age)
+console.log('is_student'+ is_student)
+
+// chapter 2
+
+var welcomeMessage = "Welcome to our webpage! \n\nThank you for visiting.";
+alert(welcomeMessage);
+
+//Assignment 2:
+// 1
+var length = 10;  
+    var width = 5; 
+
+    // Calculating the area
+    var area = length * width;
+
+    
+    console.log("The area of the rectangle is: " + area);
+// 2 
+
+var userName = prompt('pls add your name ?')
+
+alert('hello,' + userName +'welcome to our website ' )
+
+
